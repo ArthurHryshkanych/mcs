c421a4b3969aadc08d333bd0221d71a3e4268a27
ArthurHryshkanych <164743247+ArthurHryshkanych@users.noreply.github.com>
2024-05-26 23:58:14 +0300

    Add files via upload

---
+++ lab3/src/ACC.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:16:45 04/29/2024 
+-- Design Name: 
+-- Module Name:    ACC - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ACC is
+    Port ( WR   : in  STD_LOGIC;
+           RESET  : in  STD_LOGIC;
+           CLK  : in  STD_LOGIC;
+           INPUT : in  STD_LOGIC_VECTOR (7 downto 0);
+           OUTPUT : out  STD_LOGIC_VECTOR (7 downto 0));
+end ACC;
+
+architecture ACC_arch of ACC is
+    signal DATA : STD_LOGIC_VECTOR (7 downto 0);
+begin
+    process (CLK)
+    begin
+        if rising_edge(CLK) then
+            if RESET = '1' then
+                DATA <= (others => '0'); 
+            elsif WR = '1' then
+                DATA <= INPUT; 
+            end if;
+        end if;
+    end process;
+
+   
+    OUTPUT <= DATA;
+
+end ACC_arch;
